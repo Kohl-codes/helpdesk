995f4444bd6dde134b10cb0161f9228d4e63045b
Kohl <mikaorador@gmail.com>
2024-09-18 20:07:53 +0800

    add models

---
+++ app/Models/Call.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Call extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['caller_id', 'operator_id', 'equipment_id', 'description', 'call_time'];
+
+    // A call is made by one user (caller)
+    public function caller()
+    {
+        return $this->belongsTo(User::class, 'caller_id');
+    }
+
+    // A call is handled by one helpdesk operator
+    public function operator()
+    {
+        return $this->belongsTo(User::class, 'operator_id');
+    }
+
+    // A call is related to one piece of equipment
+    public function equipment()
+    {
+        return $this->belongsTo(Equipment::class);
+    }
+}
